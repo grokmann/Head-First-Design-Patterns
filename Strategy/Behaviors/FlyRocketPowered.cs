03793b6fb722ae55a92f48c1bdc9268252d83132
grokmann <grokmann@gmail.com>
2014-12-14 18:38:50 -0500

    Added FlyRocketPowered.cs (wasn't being tracked).

---
+++ Strategy/Behaviors/FlyRocketPowered.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimUDuck.Behaviors
+{
+    public class FlyRocketPowered : IFlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I'm flying with a rocket!");
+        }
+    }
+}
