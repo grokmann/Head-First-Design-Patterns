6a07077210c0337d813b4e51f27c5c53fbe2e964
grokmann <grokmann@gmail.com>
2014-12-13 20:42:53 -0500

    SimUDuck: initial commit (page 2)

---
+++ Strategy/Duck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Duck
+    {
+        public void quack() {
+            Console.Write("Quack!");
+        }
+
+        public void swim()
+        {
+            Console.Write("Duck swims!");
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Duck displays!");
+        }
+    }
+
+    class MallardDuck : Duck
+    {
+        public void display()
+        {
+            Console.WriteLine("MallardDuck displays!");
+        }
+    }
+
+    class RedheadDuck : Duck
+    {
+        public void display()
+        {
+            Console.WriteLine("RedheadDuck displays!");
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"~~~~~~~~~~~~ Begin Strategy Pattern ~~~~~~~~~~~~");
+
+            Duck mallard = new MallardDuck();
+            Duck redhead = new RedheadDuck();
+            
+            mallard.display();
+            redhead.display();
+
+            Console.ReadKey();
+        }
+    }
+}
