0598386b39af4290d9ce43e4371ed3b8fecc3c1a
grokmann <grokmann@gmail.com>
2014-12-15 11:54:10 -0500

    Initialize Ch2 Observer

---
+++ Ch2 Observer/WeatherStation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WeatherStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~~~~~~~~~~~ Begin Weather-O-Rama Weather Station ~~~~~~~~~~~~\r\n");
+
+            Console.ReadKey();
+        }
+    }
+}
