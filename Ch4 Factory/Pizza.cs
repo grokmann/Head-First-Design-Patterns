b1eff99abd9310bb1ee9f9aa76aa054580b2a9c6
grokmann <grokmann@gmail.com>
2015-01-10 14:51:50 -0500

    p. 114: Pizza shop makes one type of pizza.

---
+++ Ch4 Factory/Pizza.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Factory
+{
+    class Pizza
+   {
+        public Pizza() {
+            
+        }
+        public void Prepare()
+        {
+            Console.WriteLine("Preparing pizza...");
+        }
+
+        public void Bake()
+        {
+            Console.WriteLine("Baking pizza...");
+        }
+
+        public void Cut()
+        {
+            Console.WriteLine("Cutting pizza...");
+        }
+
+        public void Box()
+        {
+            Console.WriteLine("Boxing pizza...");
+        }
+    }
+}
---
+++ Ch4 Factory/PizzaFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_Factory
+{
+    class PizzaFactory
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = orderPizza();
+
+            Console.ReadKey();
+        }
+
+        private static Pizza orderPizza()
+        {
+            Pizza pizza = new Pizza();
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+    }
+}
