bbff0617b0fa2670cb9a81d42256eac1c400cbe7
grokmann <grokmann@gmail.com>
2015-01-13 14:04:05 -0500

    Injected factory into Pizzeria.

--- Ch4 Factory/Pizzeria.cs
+++ Ch4 Factory/Pizzeria.cs
@@ -6,20 +6,28 @@ namespace Ch4_Factory
 {
     class Pizzeria
     {
+        SimplePizzaFactory factory;
+
+        public Pizzeria(SimplePizzaFactory factory)
+        {
+            this.factory = factory;
+        }
+
         static void Main(string[] args)
         {
-            Pizza pizza1 = OrderPizza("cheese");
-            Pizza pizza2 = OrderPizza("greek");
-            Pizza pizza3 = OrderPizza("pepperoni");
-            Pizza pizza4 = OrderPizza("clam");
-            Pizza pizza5 = OrderPizza("veggie");
+            Pizzeria pizzeria = new Pizzeria(new SimplePizzaFactory());
+
+            Pizza pizza1 = pizzeria.OrderPizza("cheese");
+            Pizza pizza2 = pizzeria.OrderPizza("greek");
+            Pizza pizza3 = pizzeria.OrderPizza("pepperoni");
+            Pizza pizza4 = pizzeria.OrderPizza("clam");
+            Pizza pizza5 = pizzeria.OrderPizza("veggie");
 
             Console.ReadKey();
         }
 
-        private static Pizza OrderPizza(string pizzaType)
+        private Pizza OrderPizza(string pizzaType)
         {
-            SimplePizzaFactory factory = new SimplePizzaFactory();
             Pizza pizza = factory.CreatePizza(pizzaType);
 
             pizza.Prepare();
