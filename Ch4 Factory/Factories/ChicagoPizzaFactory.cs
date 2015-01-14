97cf2a7b0568d2a414e4945615ead45f9f275b27
grokmann <grokmann@gmail.com>
2015-01-14 08:22:35 -0500

    Added untracked files.

---
+++ Ch4 Factory/Factories/ChicagoPizzaFactory.cs
@@ -0,0 +1,38 @@
+﻿using Ch4_Factory.Pizzas;
+using System;
+
+namespace Ch4_Factory.Factories
+{
+    class ChicagoPizzaFactory : SimplePizzaFactory
+    {
+        public override Pizza CreatePizza(string type)
+        {
+            Pizza pizza = null;
+
+            Console.WriteLine("\nChicago-Style pizza coming up!");
+
+            if (type.Equals("cheese"))
+            {
+                pizza = new CheesePizza();
+            }
+            else if (type.Equals("greek"))
+            {
+                pizza = new GreekPizza();
+            }
+            else if (type.Equals("pepperoni"))
+            {
+                pizza = new PepperoniPizza();
+            }
+            else if (type.Equals("clam"))
+            {
+                pizza = new ClamPizza();
+            }
+            else if (type.Equals("veggie"))
+            {
+                pizza = new VeggiePizza();
+            }
+
+            return pizza;
+        }
+    }
+}
