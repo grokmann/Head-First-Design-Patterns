0e7f7c4bbe4d4a08b46fa7419070bd94b1b49688
grokmann <grokmann@gmail.com>
2015-01-16 15:22:40 -0500

    Added GarageDoorOpenCommand.

---
+++ Ch6 Command/GarageDoor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ch6_Command
+{
+    class GarageDoor
+    {
+        public void Up()
+        {
+            Console.WriteLine("Garage door is up.");
+        }
+
+        public void Down()
+        {
+            Console.WriteLine("Garage door is down.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Garage door is stopped.");
+        }
+
+        public void LightOn()
+        {
+            Console.WriteLine("Garage door light is on.");
+        }
+
+        public void LightOff()
+        {
+            Console.WriteLine("Garage door light is off.");
+        }
+    }
+}
---
+++ Ch6 Command/GarageDoorOpenCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ch6_Command
+{
+    class GarageDoorOpenCommand : ICommand
+    {
+        GarageDoor garageDoor;
+
+        public GarageDoorOpenCommand(GarageDoor garageDoor)
+        {
+            this.garageDoor = garageDoor;
+        }
+
+        public void Execute()
+        {
+            garageDoor.Up();
+        }
+    }
+}
