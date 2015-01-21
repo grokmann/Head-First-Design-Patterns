d037be48da5f11b2ee2f666f0e08114eb6bb787b
grokmann <grokmann@gmail.com>
2015-01-21 13:42:27 -0500

    Abstracting PrepareRecipe() in CaffeineBeverage class.

---
+++ Ch8 Template Method Pattern/CaffeineBeverage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_Template_Method_Pattern
+{
+    public abstract class CaffeineBeverage
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+        }
+
+        public abstract void Brew();
+
+        public abstract void AddCondiments();
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+
+    }
+}
---
+++ Ch8 Template Method Pattern/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ch8_Template_Method_Pattern
+{
+    public class Coffee : CaffeineBeverage
+    {
+        public override void Brew()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+    }
+}
