526b19dbcf8a74a6e63b668d4bfe5a422292c21e
grokmann <grokmann@gmail.com>
2015-01-23 16:11:30 -0500

    Added IWaitress and Alice. Alice needs to use 2 separate loops.

---
+++ Ch9 Iterator/Alice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace Ch9_Iterator
+{
+    public class Alice : IWaitress
+    {
+        public bool IsItemVegetarian(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintBreakfastMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintLunchMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintMenu()
+        {
+            PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+            ArrayList breakfastItems = pancakeHouseMenu.GetMenuItems();
+
+            DinerMenu dinerMenu = new DinerMenu();
+            MenuItem[] lunchItems = dinerMenu.GetMenuItems();
+
+            for (int i = 0; i < breakfastItems.Count; i++)
+            {
+                MenuItem menuItem = (MenuItem)breakfastItems[i];
+                Console.Write(menuItem.GetName() + " ");
+                Console.WriteLine(menuItem.GetPrice() + " ");
+                Console.WriteLine(menuItem.GetDescription() + " ");
+            }
+
+            for (int i = 0; i < breakfastItems.Count; i++)
+            {
+                MenuItem menuItem = lunchItems[i];
+                Console.Write(menuItem.GetName() + " ");
+                Console.WriteLine(menuItem.GetPrice() + " ");
+                Console.WriteLine(menuItem.GetDescription() + " ");
+            }
+        }
+
+        public void PrintVegetarianMenu()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ch9 Iterator/IWaitress.cs
@@ -0,0 +1,15 @@
+﻿namespace Ch9_Iterator
+{
+    public interface IWaitress
+    {
+        void PrintMenu();
+
+        void PrintBreakfastMenu();
+
+        void PrintLunchMenu();
+
+        void PrintVegetarianMenu();
+
+        bool IsItemVegetarian(string name);
+    }
+}
