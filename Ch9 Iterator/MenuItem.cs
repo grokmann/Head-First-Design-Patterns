9e06f250cd2b531b449bdfc098bf3c53ea4767fa
grokmann <grokmann@gmail.com>
2015-01-23 11:48:58 -0500

    Menus and MenuItem added to stage the problem.

---
+++ Ch9 Iterator/MenuItem.cs
@@ -0,0 +1,41 @@
+﻿namespace Ch9_Iterator
+{
+    public class MenuItem
+    {
+        string name;
+        string description;
+        bool vegetarian;
+        double price;
+
+        public MenuItem(string name,
+                        string description,
+                        bool vegetarian,
+                        double price)
+        {
+            this.name = name;
+            this.description = description;
+            this.vegetarian = vegetarian;
+            this.price = price;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public double GetPrice()
+        {
+            return price;
+        }
+
+        public bool IsVegetarian()
+        {
+            return vegetarian;
+        }
+    }
+}
---
+++ Ch9 Iterator/PancakeHouseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace Ch9_Iterator
+{
+    public class PancakeHouseMenu
+    {
+        ArrayList menuItems;
+
+        public PancakeHouseMenu()
+        {
+            menuItems = new ArrayList();
+
+            AddItem("K&B's Pancake Breakfast",
+                "Pancakes with scrambled eggs, and toast",
+                false,
+                2.99);
+
+            AddItem("Regular Pancake Breakfast",
+                "Pancakes with fried eggs, sausage",
+                false,
+                2.99);
+
+            AddItem("Blueberry Pancakes",
+                "Pancakes made with fresh blueberries",
+                true,
+                3.49);
+
+            AddItem("Waffles",
+                "Waffles, with your choice of blueberries or strawberries",
+                true,
+                3.59);
+        }
+
+        public void AddItem(string name, string description, bool vegetarian, double price)
+        {
+            MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+            menuItems.Add(menuItem);
+        }
+
+        public ArrayList GetMenuItems()
+        {
+            return menuItems;
+        }
+
+        //other menu methods here depending on ArrayList implementation
+    }
+}
