8823f986d58ed2f2108c41c15449976b03d9f4bc
grokmann <grokmann@gmail.com>
2015-01-24 12:37:20 -0500

    Added CafeMenu, which uses Hashtable, and CafeMenuIterator.

---
+++ Ch9 Iterator/CafeMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Ch9_Iterator
+{
+    class CafeMenu
+    {
+        Hashtable menuItems = new Hashtable();
+
+        public CafeMenu()
+        {
+            AddItem("Veggie Burger and Air Fries",
+            "Veggie burger on a whole wheat bun, lettuce, tomato, and fries",
+            true, 3.99);
+
+            AddItem("Soup of the day",
+                "A cup of the soup of the day, with a side salad",
+                false, 3.69);
+
+            AddItem("Burrito",
+                "A large burrito, with whole pinto beans, salsa, guacamole",
+                true, 4.29);
+
+        }
+
+        public void AddItem(string name, string description, bool vegetarian, double price)
+        {
+            MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+            menuItems.Add(menuItem.GetName(), menuItem);
+        }
+
+        public Iterator<MenuItem> CreateIterator()
+        {
+            return new CafeMenuIterator(menuItems);
+        }
+
+        //other menu methods here depending on Array implementation
+    }
+}
---
+++ Ch9 Iterator/CafeMenuIterator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Ch9_Iterator
+{
+    public class CafeMenuIterator  : Iterator<MenuItem>
+    {
+        Hashtable items;
+        int position = 0;
+
+        public CafeMenuIterator(Hashtable items)
+        {
+            this.items = items;
+        }
+
+        public MenuItem Next()
+        {
+            MenuItem menuItem = (MenuItem)items[position];
+            position = position + 1;
+            return menuItem;
+        }
+
+        public bool HasNext()
+        {
+            if (position >= items.Count)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
