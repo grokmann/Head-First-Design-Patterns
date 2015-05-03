687b31598968878d6913cb50f9f5b50607247547
grokmann <grokmann@gmail.com>
2015-05-03 11:48:39 -0400

    Set up for Chapter 10: State Pattern

---
+++ Ch10 State/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ch10_State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
