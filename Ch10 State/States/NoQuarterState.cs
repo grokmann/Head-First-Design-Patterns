a84e3863a60fe2a658907379fb7f5f7604b60032
grokmann <grokmann@gmail.com>
2015-12-14 22:55:19 -0500

    Added NoQuarterState (wasn't being tracked)

---
+++ Ch10 State/States/NoQuarterState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10_State.States
+{
+    public class NoQuarterState : IState
+    {
+        private GumballMachine gumballMachine;
+
+        public NoQuarterState(GumballMachine gumballMachine)
+        {
+            this.gumballMachine = gumballMachine;
+        }
+
+        public void InsertQuarter()
+        {
+            Console.WriteLine("You inserted a quarter");
+            gumballMachine.SetState(gumballMachine.GetHasQuarterState());
+        }
+
+        public void EjectQuarter()
+        {
+            Console.WriteLine("You haven't inserted a quarter");
+        }
+
+        public void TurnCrank()
+        {
+            Console.WriteLine("You turned, but there's no quarter");
+        }
+
+        public void Dispense()
+        {
+            Console.WriteLine("You need to pay first");
+        }
+    }
+}
